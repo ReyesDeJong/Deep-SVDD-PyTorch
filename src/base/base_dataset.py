c2a32517927887704e6a50687ba5c5bfcf26f498
ReyesDeJong <reyes.dejong@gmail.com>
2019-06-24 17:19:58 -0400

    validation set coded, but not best model

--- src/base/base_dataset.py
+++ src/base/base_dataset.py
@@ -25,5 +25,11 @@ class BaseADDataset(ABC):
         """Implement data loaders of type torch.utils.data.DataLoader for train_set and test_set."""
         pass
 
+    @abstractmethod
+    def val_loaders(self, batch_size: int, shuffle_train=True, shuffle_test=False, num_workers: int = 0) -> (
+            DataLoader, DataLoader):
+        """Implement data loaders of type torch.utils.data.DataLoader for train_set and test_set."""
+        pass
+
     def __repr__(self):
         return self.__class__.__name__
